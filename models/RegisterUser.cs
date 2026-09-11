4be9a4a36bbd668441e89e75eccea6fa016a1daa
Brayan Velasco <bvelascor@unal.edu.co>
2026-09-10 19:12:32 -0500

    chore: Add RegisterUser to split the model

---
+++ models/RegisterUser.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+public class RegisterUser
+{
+    [Required]
+    [RegularExpression
+        (@"^[\p{L} ]+$",
+        ErrorMessage = "El nombre solo puede contener letras y espacios.")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    [RegularExpression
+        (@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+        ErrorMessage = "La contraseña debe tener al menos 8 caracteres, incluyendo una letra mayúscula, una letra minúscula, un número y un carácter especial.")]
+    public string PasswordHash { get; set; } = string.Empty;
+    public string PrivateNote { get; set; } = string.Empty;
+}
--- models/User.cs
+++ models/User.cs
@@ -3,18 +3,8 @@ public class User
 {
     [Key]
     public int Id { get; set; }
-    [Required]
-    [RegularExpression
-        (@"^[\p{L} ]+$",
-        ErrorMessage = "El nombre solo puede contener letras y espacios.")]
     public string Name { get; set; } = string.Empty;
-
-    [Required]
-    [EmailAddress]
     public string Email { get; set; } = string.Empty;
-    [Required]
-    [RegularExpression
-        (@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-        ErrorMessage = "La contraseña debe tener al menos 8 caracteres, incluyendo una letra mayúscula, una letra minúscula, un número y un carácter especial.")]
     public string PasswordHash { get; set; } = string.Empty;
+    public string? EncryptedPrivateNote { get; set; }
 }
